3832d83bc8ea0c1ae1d875b4f73b20f810c39639
Artem Golicyn <win.rus44@gmail.com>
2023-04-01 16:32:04 +0300

    Refactored db statements as ORM-styled

--- src/app/crud/title.py
+++ src/app/crud/title.py
@@ -1,26 +1,22 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import text
+from sqlalchemy import select
 from ..schema.country_schm import CountryDate, EntityTitles
+from app.db.session import ConsolidatedData, NewsTitle
 
 
 async def get_daily_results(session: AsyncSession, item: CountryDate):
-    res = await session.execute(text(""
-                                     "SELECT entities#> '{%s}' FROM cons_data " % item.country
-                                     + f"WHERE date = date('{item.date}') "
-                                       f"AND entities ? '{item.country}'"
-                                     ))
-    result = res.scalars().all()[-1]
+    sel = await session.execute(
+        select(ConsolidatedData.entities[item.country])
+        .where(ConsolidatedData.date == item.date)
+        .where(ConsolidatedData.entities.has_key(item.country))
+    )
+    result = sel.scalars().all()[-1]
     return result
 
 
 async def get_entity_titles(session: AsyncSession, entities: EntityTitles):
-    ents = tuple(entities.entities)
-    if len(entities.entities) == 1:
-        ents = tuple(entities.entities * 2)
-
-    res = await session.execute(text(
-        "SELECT DISTINCT data FROM news_title "
-        f"WHERE id IN {ents}"
-    ))
-
-    return res.scalars().all()
+    sel = await session.execute(
+        select(NewsTitle.data)
+        .where(NewsTitle.id.in_(entities.entities))
+    )
+    return sel.scalars().all()
